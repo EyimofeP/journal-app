2b32ddbe3e300cf237613010387d4fc4df79e490
Eyimofe Ayo Pinnick <eyimofe36@gmail.com>
2021-01-29 20:39:47 +0100

    Removed Secret Key

--- journal/settings.py
+++ journal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$_h)zdd5ku^dj4a4g2$==7$@4pvwhhll1%^s8b8_uq#cf$z!h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
